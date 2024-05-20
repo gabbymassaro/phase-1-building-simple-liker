4b01a218a078d5edc470bf71483173508345c169
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-20 10:17:13 -0400

    get the hearts to turn red on 'like'

--- main.js
+++ main.js
@@ -11,50 +11,66 @@ const hearts = document.getElementsByClassName("like-glyph")
 hearts[0].addEventListener('click', function() {
   mimicServerCall()
     .then(function(response) {
-      console.log(response, "I've been clicked!")
+      document.getElementById("modal").hidden = true
+      hearts[0].innerHTML = FULL_HEART
+      hearts[0].classList.add("activated-heart")
     })
     .catch(function(error){
       console.log(error, "there's been an error!")
+      document.getElementById("modal").hidden = false
+      hearts[0].classList.add("hidden")
     })
 })
 
 hearts[1].addEventListener('click', function() {
   mimicServerCall()
     .then(function(response) {
-      console.log(response, "I've been clicked!")
+      document.getElementById("modal").hidden = true
+      hearts[1].innerHTML = FULL_HEART
+      hearts[1].classList.add("activated-heart")
     })
     .catch(function(error){
       console.log(error, "there's been an error!")
+      document.getElementById("modal").hidden = false
     })
 })
 
 hearts[2].addEventListener('click', function() {
   mimicServerCall()
     .then(function(response) {
-      console.log(response, "I've been clicked!")
+      document.getElementById("modal").hidden = true
+      hearts[2].innerHTML = FULL_HEART
+      hearts[2].classList.add("activated-heart")
     })
     .catch(function(error){
       console.log(error, "there's been an error!")
+      document.getElementById("modal").hidden = false
     })
 })
 
 hearts[3].addEventListener('click', function() {
   mimicServerCall()
     .then(function(response) {
-      console.log(response, "I've been clicked!")
+      document.getElementById("modal").hidden = true
+      hearts[3].innerHTML = FULL_HEART
+      hearts[3].classList.add("activated-heart")
     })
     .catch(function(error){
       console.log(error, "there's been an error!")
+      document.getElementById("modal").hidden = false
     })
 })
 
 hearts[4].addEventListener('click', function() {
   mimicServerCall()
     .then(function(response) {
-      console.log(response, "I've been clicked!")
+      document.getElementById("modal").hidden = true
+      hearts[4].innerHTML = FULL_HEART
+      hearts[4].classList.add("activated-heart")
     })
     .catch(function(error){
       console.log(error, "there's been an error!")
+      document.getElementById("modal").hidden = false
     })
 })
 
