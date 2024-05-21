ff4bbadcb859c416a287ada6c64ebba5cbf53926
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-20 21:51:00 -0400

    click on/off hearts to turn red and fix setTimeout

--- main.js
+++ main.js
@@ -1,85 +1,110 @@
-// Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
-// Your JavaScript code goes here!
 
 const errorModal = document.getElementById("modal")
 errorModal.classList.add("hidden")
 
 const hearts = document.getElementsByClassName("like-glyph")
 
-hearts[0].addEventListener('click', function() {
+hearts[0].addEventListener('click', (e) => {
   mimicServerCall()
     .then(function(response) {
-      errorModal.classList.add("hidden")
-      hearts[0].innerHTML = FULL_HEART
-      hearts[0].classList.add("activated-heart")
+      hearts[0] = EMPTY_HEART
+      if (hearts[0].innerText === EMPTY_HEART) {
+        hearts[0].innerText = FULL_HEART
+        hearts[0].classList.add("activated-heart")
+      } else {
+        hearts[0].innerText = EMPTY_HEART
+        hearts[0].classList.remove("activated-heart")
+      }
     })
     .catch(function(error){
       errorModal.classList.remove("hidden")
-      setTimeout(errorModal.classList.remove("hidden"), 3000)
-      hearts[0].innerHTML = EMPTY_HEART
-      hearts[0].classList.remove("activated-heart")
+      setTimeout(() => {
+        errorModal.classList.add("hidden")
+      }, 3000)
     })
 })
 
 hearts[1].addEventListener('click', function() {
   mimicServerCall()
-    .then(function(response) {
-      errorModal.classList.add("hidden")
-      hearts[1].innerHTML = FULL_HEART
+  .then(function(response) {
+    hearts[1] = EMPTY_HEART
+    if (hearts[1].innerText === EMPTY_HEART) {
+      hearts[1].innerText = FULL_HEART
       hearts[1].classList.add("activated-heart")
-    })
-    .catch(function(error){
-      setTimeout(errorModal.classList.remove("hidden"), 3000)
-      hearts[1].innerHTML = EMPTY_HEART
+    } else {
+      hearts[1].innerText = EMPTY_HEART
       hearts[1].classList.remove("activated-heart")
-    })
+    }
+  })
+  .catch(function(error){
+    errorModal.classList.remove("hidden")
+    setTimeout(() => {
+      errorModal.classList.add("hidden")
+    }, 3000)
+  })
 })
 
 hearts[2].addEventListener('click', function() {
   mimicServerCall()
-    .then(function(response) {
-      errorModal.classList.add("hidden")
-      hearts[2].innerHTML = FULL_HEART
+  .then(function(response) {
+    hearts[2] = EMPTY_HEART
+    if (hearts[2].innerText === EMPTY_HEART) {
+      hearts[2].innerText = FULL_HEART
       hearts[2].classList.add("activated-heart")
-    })
-    .catch(function(error){
-      setTimeout(errorModal.classList.remove("hidden"), 3000)
-      hearts[2].innerHTML = EMPTY_HEART
+    } else {
+      hearts[2].innerText = EMPTY_HEART
       hearts[2].classList.remove("activated-heart")
-    })
+    }
+  })
+  .catch(function(error){
+    errorModal.classList.remove("hidden")
+    setTimeout(() => {
+      errorModal.classList.add("hidden")
+    }, 3000)
+  })
 })
 
 hearts[3].addEventListener('click', function() {
   mimicServerCall()
-    .then(function(response) {
-      errorModal.classList.add("hidden")
-      hearts[3].innerHTML = FULL_HEART
+  .then(function(response) {
+    hearts[3] = EMPTY_HEART
+    if (hearts[3].innerText === EMPTY_HEART) {
+      hearts[3].innerText = FULL_HEART
       hearts[3].classList.add("activated-heart")
-    })
-    .catch(function(error){
-      setTimeout(errorModal.classList.remove("hidden"), 3000)
-      hearts[3].innerHTML = EMPTY_HEART
+    } else {
+      hearts[3].innerText = EMPTY_HEART
       hearts[3].classList.remove("activated-heart")
-    })
+    }
+  })
+  .catch(function(error){
+    errorModal.classList.remove("hidden")
+    setTimeout(() => {
+      errorModal.classList.add("hidden")
+    }, 3000)
+  })
 })
 
 hearts[4].addEventListener('click', function() {
   mimicServerCall()
-    .then(function(response) {
-      errorModal.classList.add("hidden")
-      hearts[4].innerHTML = FULL_HEART
+  .then(function(response) {
+    hearts[4] = EMPTY_HEART
+    if (hearts[4].innerText === EMPTY_HEART) {
+      hearts[4].innerText = FULL_HEART
       hearts[4].classList.add("activated-heart")
-    })
-    .catch(function(error){
-      setTimeout(errorModal.classList.remove("hidden"), 3000)
-      hearts[4].innerHTML = EMPTY_HEART
+    } else {
+      hearts[4].innerText = EMPTY_HEART
       hearts[4].classList.remove("activated-heart")
-    })
+    }
+  })
+  .catch(function(error){
+    errorModal.classList.remove("hidden")
+    setTimeout(() => {
+      errorModal.classList.add("hidden")
+    }, 3000)
+  })
 })
-
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
